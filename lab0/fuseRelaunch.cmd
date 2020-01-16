325f4bb63f796c9c7cc763d92259078d0f88db14
Rob Royce <robertris2@gmail.com>
2020-01-16 07:30:24 -0800

    Pruned files based on new gitignore

--- lab0/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/parallels/ucla/csm152a/labs/csm152a/lab0/counter_TB_isim_beh.exe" -prj "/home/parallels/ucla/csm152a/labs/csm152a/lab0/counter_TB_beh.prj" "work.counter_TB" "work.glbl" 
--- lab_1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/parallels/ucla/m152acs/labs/csm152a/lab_1/priority_encoder_TB_isim_beh.exe" -prj "/home/parallels/ucla/m152acs/labs/csm152a/lab_1/priority_encoder_TB_beh.prj" "work.priority_encoder_TB" "work.glbl" 
