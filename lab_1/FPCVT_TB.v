b2cea6c604722c6dc040a210f2457f0ae614545f
Rob Royce <robertris2@gmail.com>
2020-01-20 16:33:29 -0800

    Added more tests to verify examples in lab handout where valid and a few more edge cases

--- lab_1/FPCVT_TB.v
+++ lab_1/FPCVT_TB.v
@@ -51,41 +51,37 @@ module FPCVT_TB;
       // Add stimulus here
       forever
 	begin
-	   // Begin provided test cases
-	   $display("Begin provided test cases");	   
-	   #10 D <= 12'b0111_0100_0000; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
-	   #10 D <= 12'b0111_1111_1111; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
-	   #10 D <= 12'b1000_0000_0010; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
-	   #10 D <= 12'b1111_1010_0101; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
-	   #10 D <= 12'b1000_1000_1010; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
-	   #10 D <= 12'b1001_1100_1111; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
-	   #10 D <= 12'b0111_1111_1111; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b", $time, D[11:8], D[7:4], D[3:0], S, E, F);
 
+	   $display("Begin provided test cases");	   
+	   #10 D <= 12'b0111_0100_0000; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b | Expected: 0_111_1111", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b0111_1111_1111; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b | Expected: 0_111_1111", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b1000_0000_0010; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b | Expected: 1_111_1111", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b1000_1000_1010; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b | Expected: 1_111_1111", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b1001_1100_1111; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b | Expected: 1_111_1100", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b1111_1010_0101; #1 $display("%t | D: %b_%b_%b | SEF: %b_%b_%b | Expected: 1_011_1011", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+
+	   $display("Begin test cases derived from the lab handout");
+	   #10 D <= 12'd422; #1 $display("%t | D: 422 | SEF:%b_%b_%b | In Float: %d | Expected: 416", $time, S, E, F, F*(2**E));
+	   #10 D <= -12'd422; #1 $display("%t | D: -422 | SEF:%b_%b_%b | In Float: %d | Expected: 416", $time, S, E, F, F*(2**E));
+	   #10 D <= 12'b0000_0010_1100; #1 $display("%t | D: %b_%b_%b | SEF:%b_%b_%b | Expected: 0_010_1011", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b0000_0010_1101; #1 $display("%t | D: %b_%b_%b | SEF:%b_%b_%b | Expected: 0_010_1011", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b0000_0010_1110; #1 $display("%t | D: %b_%b_%b | SEF:%b_%b_%b | Expected: 0_010_1100", $time, D[11:8], D[7:4], D[3:0], S, E, F);	   
+	   #10 D <= 12'b0000_0010_1111; #1 $display("%t | D: %b_%b_%b | SEF:%b_%b_%b | Expected: 0_010_1100", $time, D[11:8], D[7:4], D[3:0], S, E, F);
+	   #10 D <= 12'b0000_0111_1111; #1 $display("%t | D: %b_%b_%b | SEF:%b_%b_%b | Expected: 0_100_1000", $time, D[11:8], D[7:4], D[3:0], S, E, F);	   	   
+	   
 	   // Begin custom test cases
 	   $display("Begin custom test cases");
-	   #10 D <= 12'b0000_0000_0000;
-	   #10 D <= 12'b0000_1111_0000;
-	   #10 D <= 12'b1111_0000_1111;
-	   #10 D <= 12'b1111_1111_1111;
-	   #10 D <= 12'b0111_1111_1111;
-	   #10 D <= 12'b0000_0000_0000;
-	   #10 D <= 12'b1000_0000_0000;
+	   #10 D <= 12'd0; #1 $display("%t | D: 0 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);
+	   #10 D <= -12'd1; #1 $display("%t | D: -1 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);	   
+	   #10 D <= 12'd1; #1 $display("%t | D: 1 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);
+	   #10 D <= -12'd2048; #1 $display("%t | D: -2048 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);
+	   #10 D <= -12'd2047; #1 $display("%t | D: -2047 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);
+	   #10 D <= 12'd2047; #1 $display("%t | D: 2047 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);
+	   #10 D <= 12'd422; #1 $display("%t | D: 422 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);
+	   #10 D <= -12'd422; #1 $display("%t | D: -422 | SEF: %b_%b_%b | Float: %s%d", $time, S, E, F, (S == 1'b1) ? "-" : "+", F*2**E);	   	   	   
 	   $finish();
 	   
-	end
-
-   end
-   
-
-
-   /*   12'b0111_0100_0000	1856	8'b0111_1111
-    12'b0111_1111_1111	2047	8'b0111_1111
-    12'b1000_0000_0010
-    -2046	8'b1111_1111
-    12'b1000_1000_1010
-    -1910	8'b1111_1111
-    12'b1001_1100_1111	 -1585	8'b1111_1100 
-    12'b1111_1010_0101	 -91	8'b1011_1011 */
-   
+	end // forever begin
+   end // initial begin
 endmodule
 
