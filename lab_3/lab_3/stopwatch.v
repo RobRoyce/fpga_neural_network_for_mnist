8748c7b04b0d59e0378450db2899347fa90f546d
Tyler the Destroyer <ins10102@gmail.com>
2020-02-04 10:18:03 -0800

    Initial Lab 3

---
+++ lab_3/lab_3/stopwatch.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:57:53 02/04/2020 
+// Design Name: 
+// Module Name:    stopwatch 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module stopwatch(
+    btn_st,            // Start/Stop
+    btn_rst,           // Reset
+    clk,
+    );
+    
+    
+    parameter state_pause = 0;
+    parameter state_count = 1;
+
+
+endmodule
