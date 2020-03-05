1939ea4ea68cffcbfb2eca686c8d4e9cb2d25a53
Rob Royce <robertris2@gmail.com>
2020-03-04 16:02:06 -0800

    Added basic stuff

--- lab_3/lab_3/debouncer.v
+++ lab_3/lab_3/debouncer.v
@@ -15,46 +15,46 @@ module debouncer(input wire  clk,
                  output wire trans_up,
                  output wire trans_dn);
 
-    // Registers used to synchronize button presses to clk
-    reg sync_0, sync_1;
+   // Registers used to synchronize button presses to clk
+   reg                       sync_0, sync_1;
 
    initial begin
       sync_0 = 0;
       sync_1 = 0;
       state = 0;
-      end
+   end
 
-    // Use sync_0 and sync_1 as a sort-of master-slave configuration
-    always @(posedge clk)
-    begin
+   // Use sync_0 and sync_1 as a sort-of master-slave configuration
+   always @(posedge clk)
+     begin
         sync_0 = switch_input;
-    end
+     end
 
-    always @(posedge clk)
-    begin
+   always @(posedge clk)
+     begin
         sync_1 = sync_0;
-    end
+     end
 
 
-    // The output of the second register is used to debounce
-    reg [15:0] count;                   // 16-bit counter => 65535 states ==> 65535/100Mhz counter
-    wire idle = (state == sync_1);      // If sync_1 and state are equal, no switch transition is in progress => idle = 1
-    wire finished = &count;             // true when all bits of count are 1's (i.e. all bits of count are ANDed together)
+   // The output of the second register is used to debounce
+   reg [15:0] count;                   // 16-bit counter => 65535 states ==> 65535/100Mhz counter
+   wire       idle = (state == sync_1);      // If sync_1 and state are equal, no switch transition is in progress => idle = 1
+   wire       finished = &count;             // true when all bits of count are 1's (i.e. all bits of count are ANDed together)
 
-    always @(posedge clk)
-    begin
+   always @(posedge clk)
+     begin
         if(idle)
-        begin
-            count <= 0; // set the counter to zero when the switch is not being pressed
-        end else begin
-            count <= count + 16'b1; // start and increment the counter when the switch is being pressed
-            if (finished)
-            begin
-                state <= ~state; // Toggle state once counter reaches max value
-            end
-        end
-    end
+          begin
+             count <= 0; // set the counter to zero when the switch is not being pressed
+          end else begin
+             count <= count + 16'b1; // start and increment the counter when the switch is being pressed
+             if (finished)
+               begin
+                  state <= ~state; // Toggle state once counter reaches max value
+               end
+          end
+     end
 
-    assign trans_dn = ~idle & finished & ~state;    // output a signal when the switch transitions up
-    assign trans_up = ~idle & finished & state;     // output a signal when the switch transitions down
+   assign trans_dn = ~idle & finished & ~state;    // output a signal when the switch transitions up
+   assign trans_up = ~idle & finished & state;     // output a signal when the switch transitions down
 endmodule
